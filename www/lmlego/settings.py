e0799f8e80028af63d31e3f06707457ce7315b27
Darrel Herbst <dherbst@dramafever.com>
2012-10-09 08:08:42 -0400

    adding context processors for request

--- www/lmlego/settings.py
+++ www/lmlego/settings.py
@@ -90,6 +90,17 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    'django.core.context_processors.request',
+    "django.contrib.messages.context_processors.messages")
+
+
+
 ROOT_URLCONF = 'lmlego.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
